b154d68e944c60d351d6d610f5cfd5c8a35b02eb
vdmehta36 <vdmehta36@gmail.com>
2018-07-23 21:12:29 +0530

    registration code change

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { LoginService } from '../../services/login.service';
+import { FormGroup, FormBuilder, Validators } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,16 +10,35 @@ import { LoginService } from '../../services/login.service';
 })
 export class RegisterComponent implements OnInit {
   user: User;
-  constructor(private loginService: LoginService) {}
+  form: FormGroup;
+  private formSubmitAttempt: boolean; 
+  constructor(private loginService: LoginService,private fb: FormBuilder) {}
 
   ngOnInit() {
     this.user = new User('', '', '', '', '', '', '', '');
+    this.form = this.fb.group({     // {5}
+      name: ['', Validators.required],
+      pincode: ['', Validators.required],
+      location: ['', Validators.required],
+      district: ['', Validators.required],
+      partner: ['', Validators.required]
+    });
   }
 
+  isFieldInvalid(field: string) { // {6}
+  return (
+    (!this.form.get(field).valid && this.form.get(field).touched) ||
+    (this.form.get(field).untouched && this.formSubmitAttempt)
+  );
+}
   onSubmit() {
-  this.loginService.register(this.user).then(res => {
-    console.log(res);
-//    this.loginService.navigateToLogin();
-  });
+    if (this.form.valid) {
+      console.log(this.form.value); // {7}
+      this.loginService.register(this.form.value).then(res => {
+        console.log(res);
+      });
+    }
+    this.formSubmitAttempt = true;             // {8}
+ 
   }
 }
